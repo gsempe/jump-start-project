a2bef351ba83a86627217758624349515654200a
Guillaume Sempe <guillaume.sempe@gmail.com>
2018-06-23 23:03:01 +0100

    Improve jump start script

--- jump_start.sh
+++ jump_start.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 # Jump_start script helps to switch between projects faster
-# layout.sh is the top level script file in which are injected other components
+# jump_start.sh is the top level script file in which are injected other
+# components.
+# JUMP_START_DIR can be set to override the default root directory
+# PROJECT_NAME is expected to be set to the name of the project
 
 # https://www.ynonperek.com/2017/09/04/bash-cheat-sheet/
 set -e
 set -o noclobber
 
-# Copy needed components here
+JUMP_START_DIR="${JUMP_START_DIR:-"~/projects"}"
+WDIR=${JUMP_START_DIR}/${PROJECT_NAME}
 
+# Copy needed components here
 
 set +e
 set +o noclobber
