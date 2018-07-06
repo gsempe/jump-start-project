82797a955087446ec28248e13a3799b951cbd1ab
Guillaume Sempe <guillaume.sempe@gmail.com>
2018-07-06 16:55:46 +0100

    Add asdf script

---
+++ asdf.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Jump_start script helps to switch between projects faster
+# https://github.com/gsempe/jump-start-project
+#
+# asdf.sh defines a function to install https://github.com/asdf-vm/asdf
+# ASDF_DIR can be set to override the destination folder
+
+ASDF_DIR=${ASDF_DIR:-${WDIR}/asdf}
+ASDF_VERSION="v0.5.0"
+
+if [ ! -d "${ASDF_DIR}" ]; then
+        step "Installing ASDF into ${ASDF_DIR}..."
+        git clone https://github.com/asdf-vm/asdf.git ${ASDF_DIR} --branch ${ASDF_VERSION}
+        cd ${ASDF_VERSION}
+else
+        step "Skipping asdf-vm/asdf clone, folder ${ASDF_DIR} exists..."
+        cd ${ASDF_DIR}
+fi
+step "Configuring ASDF..."
+source ${ASDF_DIR}/asdf.sh
+source ${ASDF_DIR}/completions/asdf.bash
